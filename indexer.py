020b3ca68d96250bc7b7289b1281de9a22ec9e9c
Wes Powell <93751858+TreeCityWes@users.noreply.github.com>
2023-09-27 21:55:35 -0400

    Update indexer.py

--- indexer.py
+++ indexer.py
@@ -1,8 +1,12 @@
 import sqlite3
 import json
 
-def count_uppercase_letters(string):
-    return sum(1 for char in string if char.isalpha() and char.isupper())
+def count_uppercase_letters(string: str) -> int:
+    """Counts the number of uppercase letters in a string"""
+    return sum(1 for char in string if char.isupper())
+
+def connect_to_db(db_path: str) -> sqlite3.Connection:
+    return sqlite3.connect(db_path, timeout=10)
 
 def create_tables(cursor):
     cursor.execute("""
@@ -16,56 +20,50 @@ def create_tables(cursor):
         block_type TEXT DEFAULT 'regular'
     )
     """)
+
+    
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS super_blocks (
         account TEXT PRIMARY KEY,
-        super_block_count INTEGER
+        super_block_count INTEGER DEFAULT 0
     )
     """)
-
-def main():
-    print("Compiling the data... This may take a while, please wait.")
-
-    source_conn = sqlite3.connect("blockchain.db")
-    source_cursor = source_conn.cursor()
-
-    index_conn = sqlite3.connect("blockchainindex.db")
-    index_cursor = index_conn.cursor()
-
-    create_tables(index_cursor)
-
-    source_cursor.execute("SELECT records_json FROM blockchain")
     
-    # Start a transaction
-    index_conn.execute('BEGIN TRANSACTION')
-
+def process_blocks(source_cursor, index_conn, index_cursor):
     processed_records = 0
     try:
-        for record in iter(source_cursor.fetchone, None):  # Using fetchone in a loop
-            records_list = json.loads(record[0])
+        source_cursor.execute("SELECT records_json FROM blockchain")
+        all_values = source_cursor.fetchall()
 
-            all_values = []
-            for item in records_list:
-                hash_to_verify = item.get("hash_to_verify")
-                key = item.get("key")
-                account = item.get("account")
-                xuni_account = item.get("xuni_account", "")
-                created_at = item.get("date")
+        for item in all_values:
+            records_json = item[0]
+            records_list = json.loads(records_json)  # Decode the JSON string to a Python list
+        
+            for record in records_list:  # Iterate over each dictionary in the list
+                if isinstance(record, dict):  # Check if the item is a dictionary
+                    hash_to_verify = record.get("hash_to_verify")
+                    key = record.get("key")
+                    account = record.get("account")
+                    xuni_account = record.get("xuni_account", "")
+                    created_at = record.get("date")
+                    is_xuni_block = 1 if record.get("xuni_id") is not None else 0
                 
-                is_xuni_block = item.get("xuni_id") is not None
-                is_super_block = count_uppercase_letters(hash_to_verify) >= 65  # Super block condition
-                
-                # Default block type is regular, unless it's a Xuni block or Super block
-                if is_xuni_block:
-                    block_type = 'xuni'
-                elif is_super_block:
-                    block_type = 'super'
+                if not is_xuni_block:
+                    is_super_block = 1 if count_uppercase_letters(hash_to_verify) >= 65 else 0
                 else:
-                    block_type = 'regular'
-                
-                all_values.append((hash_to_verify, key, account, xuni_account, created_at, block_type))
-                
-                if is_super_block:
+                    is_super_block = 0
+    
+                # Insert into blocks table and handle unique constraint failure if needed.
+                try:
+                    index_cursor.execute("""
+                    INSERT INTO blocks (hash_to_verify, key, account, xuni_account, created_at, block_type)
+                    VALUES (?, ?, ?, ?, ?, ?);
+                    """, (hash_to_verify, key, account, xuni_account, created_at, 'xuni' if is_xuni_block else 'super' if is_super_block else 'regular'))
+                except sqlite3.IntegrityError:
+                    print(f"Entry with key {key} already exists in blocks. Skipping...")
+    
+                # If the block is a super block and not a xuni block, update the super_blocks table
+                if is_super_block and not is_xuni_block:
                     index_cursor.execute("""
                     INSERT OR IGNORE INTO super_blocks (account, super_block_count)
                     VALUES (?, 0);
@@ -76,29 +74,26 @@ def main():
                     SET super_block_count = super_block_count + 1
                     WHERE account = ?;
                     """, (account,))
-                    
-            index_cursor.executemany("""
-            INSERT OR IGNORE INTO blocks (hash_to_verify, key, account, xuni_account, created_at, block_type)
-            VALUES (?, ?, ?, ?, ?, ?)
-            """, all_values)
-            
-            processed_records += len(all_values)
-
-            # Print a progress update every 1000 records processed
+   
+            processed_records += 1
             if processed_records % 1000 == 0:
                 print(f"Processed {processed_records} records so far...")
 
-        # Commit the transaction
         index_conn.commit()
 
-    except sqlite3.IntegrityError as e:
-        print(f"Integrity Error: {e}")
-        index_conn.rollback()
-    except Exception as e:
-        print(f"An error occurred: {e}")
+    except Exception as e:  # Handle the exception properly
+        print(f"An unexpected error occurred: {e}")
         index_conn.rollback()
 
-    print(f"Finished processing. Total records processed: {processed_records}.")
+def main():
+    source_conn = connect_to_db("blockchain.db")
+    source_cursor = source_conn.cursor()
+    
+    index_conn = connect_to_db("blockchainindex.db")
+    index_cursor = index_conn.cursor()
+    
+    create_tables(index_cursor)
+    process_blocks(source_cursor, index_conn, index_cursor)
     
     source_conn.close()
     index_conn.close()
